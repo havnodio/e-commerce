051436b3b0c676cb20c1ec95c7ef676534d1e7ff
[dyad] <itisdjoe@gmail.com>
2025-10-07 22:36:15 +0100

    Updated src/components/GustoIntro.tsx

--- src/components/GustoIntro.tsx
+++ src/components/GustoIntro.tsx
@@ -16,7 +16,7 @@ const GustoIntro: React.FC = () => {
 
       <div className="mt-6">
         <SplitText
-          text="vous avez aimer"
+          text="vous  avez aimer"
           tag="h2"
           className="text-3xl font-medium"
           splitType="chars"
